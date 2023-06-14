cbf95ba3d701e0972c90c191d3d7b0ac60efac0e
Dengell <9680004050@mail.ru>
2023-06-14 11:21:01 +0300

    do

--- data_create.py
+++ data_create.py
@@ -1,20 +1,20 @@
 def name_data():
-    name = input('Введите Ваше имя: ')
+    name = input('Введите имя: ')
     print('Очень красивое имя! (а меня зовут гб_бот, меня создала компания GeekBrains!')
     return name
 
 
 def surname_data():
-    surname = input('Введите Вашу фамилию: ')
+    surname = input('Введите фамилию: ')
     return surname
 
 
 def phone_data():
     # import re
-    phone = input('Введите Ваш телефон: ')
+    phone = input('Введите телефон: ')
     return phone
 
 
 def address_data():
-    address = input('Введите Ваш адрес: ')
+    address = input('Введите адрес: ')
     return address
